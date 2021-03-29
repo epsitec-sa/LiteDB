47cb62c58219d7dcd8b108ff114b65156aa81dae
Jonny Quarta <quarta@epsitec.ch>
2021-03-29 15:53:05 +0200

    Add test for normal db deletions

--- LiteDB.Tests/Engine/CorruptedTest.cs
+++ LiteDB.Tests/Engine/CorruptedTest.cs
@@ -13,18 +13,36 @@ namespace LiteDB.Tests
     public class CorruptedTest
     {
         [TestMethod]
-        public void Engine_Delete_Test()
+        public void Engine_DeleteOK_Test()
         {
-            using (var file = new TempFile("Resources/corrupted/Db_with_leak.db"))
+            using (var file = new TempFile("Resources/ok/Db_ok.db"))
             {
                 using (var db = new LiteDatabase("Filename=" + file.Filename + ";Mode=Exclusive"))
                 {
                     var colNames = db.GetCollectionNames ();
 
-                    foreach (var colName in colNames)
+                    foreach(var colName in colNames)
                     {
                         db.GetCollection (colName).Delete (Query.All ());
+                        Assert.AreEqual<int> (0, db.GetCollection (colName).Count ());
                     }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Engine_DeleteKO_Test()
+        {
+            using (var file = new TempFile ("Resources/corrupted/Db_with_leak.db"))
+            {
+                using (var db = new LiteDatabase ("Filename=" + file.Filename + ";Mode=Exclusive"))
+                {
+                    var colName = db.GetCollectionNames ()
+                        .Where ((_colName) => _colName == "AccountingPlanTemplates")
+                        .Single ();
+
+                    Assert.ThrowsException<LiteException> (() => db.GetCollection (colName).Delete (Query.All ()),
+                        "File has no content or is corrupted (Circular pageID sequence detected).");
                 }
             }
         }
