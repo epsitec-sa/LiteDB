5f822d6f9a6226259a98b66c0587e7c101254273
Jonny Quarta <quarta@epsitec.ch>
2021-03-29 13:57:27 +0200

    Add test for leaking database

---
+++ LiteDB.Tests/Engine/CorruptedTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiteDB.Tests
+{
+    [TestClass]
+    public class CorruptedTest
+    {
+        [TestMethod]
+        public void Engine_Delete_Test()
+        {
+            using (var file = new TempFile("Resources/corrupted/Db_with_leak.db"))
+            {
+                using (var db = new LiteDatabase("Filename=" + file.Filename + ";Mode=Exclusive"))
+                {
+                    var colNames = db.GetCollectionNames ();
+
+                    foreach (var colName in colNames)
+                    {
+                        db.GetCollection (colName).Delete (Query.All ());
+                    }
+                }
+            }
+        }
+    }
+}
